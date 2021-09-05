7edc09e718c58028fd050b71339edd6a48a0f9e0
Daiyuu Nobori <dnobori.gitcommit@coe.ad.jp>
2021-09-06 02:58:01 +0900

    改良さん

--- roles/common_linux/files/scripts/se_lxd_delete_snap_cache.sh
+++ roles/common_linux/files/scripts/se_lxd_delete_snap_cache.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-find / -type f -path "*/var/cache/apparmor/*" -name "snap-confine.core.*" | xargs -n 1 -P 1 -IXXX rm -f XXX
+find / -xdev -type f -path "*/var/cache/apparmor/*" -name "snap-confine.core.*" | xargs -n 1 -P 1 -IXXX rm -f XXX
+find / -xdev -type f -path "*/var/cache/apparmor/*" -name "snap-confine.snapd.*" | xargs -n 1 -P 1 -IXXX rm -f XXX
 
-find / -type f -path "*/var/cache/apparmor/*" -name "snap-snapd.core.*" | xargs -n 1 -P 1 -IXXX rm -f XXX
+find /vmdata1/ -xdev -type f -path "*/var/cache/apparmor/*" -name "snap-confine.core.*" | xargs -n 1 -P 1 -IXXX rm -f XXX
+find /vmdata1/ -xdev -type f -path "*/var/cache/apparmor/*" -name "snap-confine.snapd.*" | xargs -n 1 -P 1 -IXXX rm -f XXX
 
