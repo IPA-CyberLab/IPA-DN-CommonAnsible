22dc0f67054990203a8f2b6229d8864203a76287
Daiyuu Nobori <dnobori.gitcommit@coe.ad.jp>
2024-06-16 09:02:51 +0900

    Ubuntu 22.04 を NFS Server にする場合、 NFS Client からは Ver=4 (nfs4) で接続するべきである。
    そうしなければ、クライアント → サーバーに断続的に大量のデータが送付される場合、TCP セッションがプチプチと切れて (TCP ZeroWindow 通知が一度なされた後に、回復しない。サーバー側の TCP スタックのバグか?) 復活しなくなる。

--- roles/common_linux/files/scripts/se_connect_to_nfs.sh
+++ roles/common_linux/files/scripts/se_connect_to_nfs.sh
@@ -19,7 +19,7 @@ do
   
   mkdir -p $client_mount_point
 
-  mount -o soft,retrans=3,vers=3,tcp,timeo=600,rsize=1048576,wsize=1048576,noac,retry=0,nolock,noacl,local_lock=none -t nfs $server_ip:$server_mount_name $client_mount_point
+  mount -o soft,retrans=3,vers=4,tcp,timeo=600,rsize=1048576,wsize=1048576,noac,retry=0,nolock,noacl,local_lock=none -t nfs $server_ip:$server_mount_name $client_mount_point
   
   if (mountpoint -q $client_mount_point); then
     break
